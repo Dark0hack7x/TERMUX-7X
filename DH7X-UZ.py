828fcb224bdb6b1a177c2c32c80d471dbd56c619
Dark0hack7x <68660376+Dark0hack7x@users.noreply.github.com>
2020-07-23 15:45:08 +0300

    Add files via upload

---
+++ DH7X-UZ.py
@@ -0,0 +1,57 @@
+Bl="\033[1;30m"  #Black
+Re="\033[1;31m"  #Red
+Gr="\033[1;32m"  #Green
+Ya="\033[1;33m"  #Yallow
+bl="\033[1;34m"  #blue 
+Pu="\033[1;35m"  #purple
+cy="\033[1;36m"  #cyan
+wh="\033[1;37m"  #white
+import os
+print ( """ \033[1;35m########\033[1;36m##########\033[1;31m##########\033[1;33m#############\033[1;30m#########\033[1;32m##########
+\033[1;33mtelegram:@DARK_HACK_7X		      			    \033[1;35m#
+\033[1;31mTelegram@D_H_7X			      			    \033[1;35m#
+\033[1;32mwhats app:https://chat.whatsapp.com/DhQd4uuXGZYERcCQRZ9QDp  \033[1;35m#
+\033[1;36mblogger:Https://www.dark-hack-7x.blogspot.com		    \033[1;35m#
+				 			    \033[1;35m#
+\033[1;31m########\033[1;32m##############\033[1;33m##############\033[1;34m##############\033[1;36m###########
+			\033[1;30mD\033[1;31mA\033[1;32mR\033[1;33mK  \033[1;34mH\033[1;35mA\033[1;36mC\033[1;37mK  \033[1;30m7\033[1;31mX  \033[1;32mT\033[1;33mE\033[1;34mA\033[1;35mM 
+""")
+print ('			\033[1;33mPress \033[1;32mEnter \033[1;31mTo \033[1;34mStart... ' )
+dh7x=input('   ')
+os.system ('pkg install man')
+os.system ('pkg update && pkg upgrade')
+os.system ('pkg install python')
+os.system ('pkg install python2')
+os.system ('pkg install python3')
+os.system ('pkg install ruby')
+os.system ('pkg install git')
+os.system ('pkg install java')
+os.system ('pkg install php')
+os.system ('pkg install perl')
+os.system ('pkg install bash')
+os.system ('pkg install google')
+os.system ('pkg install clang')
+os.system ('pkg install nmap')
+os.system ('pkg install macchanger')
+os.system ('pkg install figlet ')
+os.system ('pkg install nano')
+os.system ('pkg install cowsay')
+os.system ('pkg install curl')
+os.system ('pkg install tar')
+os.system ('pkg install tor')
+os.system ('pkg install sudo')
+os.system ('pkg install zip')
+os.system ('pkg install unzip')
+os.system ('pkg install wget')
+os.system ('pkg install wcalc')
+os.system ('pkg install openssl')
+os.system ('pkg install bmon')
+os.system ('clear')
+os.system ('pkg install Hydra')
+os.system ('cd')
+os.system ('pkg install hydra')
+os.system ('clear')
+os.system ('clear')
+os.system ('clear')
+os.system ('clear')
+os.system ('cowsay -f eyes Dark Hack 7x Team')
